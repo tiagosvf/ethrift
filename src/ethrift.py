8675aae00cad0527e17d787a47151b495c4fc9f7
tiagosvf <tiagosvf@hotmail.com>
2020-07-22 17:41:51 +0100

    added ability to add and list queries

--- src/ethrift.py
+++ src/ethrift.py
@@ -2,14 +2,70 @@ import json
 import discord
 from discord.ext import commands
 
-with open('settings.json', 'r') as f:
+queries_list = []
+
+
+class Query:
+    def __init__(self, query, min_price, max_price):
+        self.query = query
+        self.min_price = min_price
+        self.max_price = max_price
+        self.items = []
+
+    def formatted_query(self):
+        return f"{self.query} | Min. Price: {self.min_price}$ | Max. Price: {self.max_price}$"
+
+    def list_queries(self):
+        result = ''
+        for query in queries_list:
+            result += f"\n{query.formatted_query()}"
+        return result
+
+
+with open("settings.json", 'r') as f:
     config = json.load(f)
-    token = config["discord"]["token"]  
+    token = config["discord"]["token"]
 
 bot = commands.Bot(command_prefix='!')
+bot.remove_command('help')
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('Pong!')
+    await ctx.send("Pong!")
+
+
+@bot.command()
+async def kill(ctx):
+    await ctx.send("Goodbye!")
+    raise SystemExit(0)
+
+
+@bot.command(aliases=["commands", "help"])
+async def cmd(ctx):
+    result = """
+    ```
+!help, !cmd or !commands -> Show this list of commands
+!ping -> Checks if bot is online
+    
+!add <\"query keywords\"> <min. price> <max. price>
+!queries -> Lists all the currently active queries
+
+!kill -> Shuts down the bot"```"""
+    await ctx.send(result)
+
+@bot.command()
+async def queries(ctx):
+    result = Query.list_queries(queries)
+    if result:
+        await ctx.send(f"```{result}```")
+    else:
+        await ctx.send("```You have not added any queries. \nAdd one using !add <\"query keywords\"> <min_price> <max_price>```")
+
+
+@bot.command()
+async def add(ctx, query, min_price, max_price):
+    _query = Query(query, min_price, max_price)
+    queries_list.append(_query)
+    await ctx.send(f"Query \"{query}\" with minimum price of {min_price}$ and maximum price of {max_price}$ added")
 
 bot.run(token)
